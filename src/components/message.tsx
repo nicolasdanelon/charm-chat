47e2ac57ca272f35c1f8ce0ac7ec4c818d24aab5
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-11-25 10:54:35 -0300

    Fix type name ✍️

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,17 +1,17 @@
-type MessageType = {
+type MessageProps = {
   avatar: string;
   name: string;
   message: string;
   time: string;
 }
 
-function Message({ avatar, name, message, time } : MessageType) {
+function Message({ avatar, name, message, time } : MessageProps) {
   return (
     <div className="flex items-start mb-4 text-sm">
       <img src={avatar} className="w-10 h-10 rounded mr-3" />
         <div className="flex-1 overflow-hidden">
           <div>
-            <span className="font-bold">{name}</span>
+            <span className="font-bold">{name}</span>{' '}
             <span className="text-grey text-xs">{time}</span>
           </div>
           <p className="text-black leading-normal">{message}</p>
