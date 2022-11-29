cabd04bfe69a34e0a364d4e6a85432739035c05e
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-11-29 00:19:20 -0300

    adding login

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from "./components/layout"
 import Home from "./pages/home"
+import Login from "./pages/login"
 
 export function App() {
   return (
@@ -8,6 +9,7 @@ export function App() {
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
       </Route>
+      <Route path="/login" element={<Login />} />
     </Routes>
   )
 }
