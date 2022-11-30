6b6dbd8c70edce593e33e68db6dd97c42ef13666
Maxi Skell <maxiskell@gmail.com>
2022-11-30 16:54:17 +0000

    🧑🏻‍💻 Add messages store

---
+++ src/stores/useMessagesStore.ts
@@ -0,0 +1,51 @@
+import create from "zustand"
+import { persist } from "zustand/middleware"
+import { supabase } from "../supabaseClient"
+
+export type Message = {
+  id: string
+  content: string
+}
+
+type MessagesStore = {
+  messages: Message[]
+  selectedChannelId: number | null
+  selectedConversationId: number | null
+  selectChannel: (id: number | null) => void
+  selectConversation: (id: number | null) => void
+}
+
+const useChannelsStore = create<MessagesStore>()(
+  persist(
+    (set) => ({
+      messages: [],
+      selectedChannelId: null,
+      selectedConversationId: null,
+      selectChannel: async (id: number | null) => {
+        set({ selectedChannelId: id, selectedConversationId: null })
+
+        const { data: messages, error } = await supabase
+          .from("messages")
+          .select("*")
+          .eq("channel_id", id)
+
+        if (error) console.error(error)
+        if (messages) set({ messages })
+      },
+      selectConversation: async (id: number | null) => {
+        set({ selectedConversationId: id, selectedChannelId: null })
+
+        const { data: messages, error } = await supabase
+          .from("messages")
+          .select("*")
+          .eq("conversation_id", id)
+
+        if (error) console.error(error)
+        if (messages) set({ messages })
+      },
+    }),
+    { name: "messagesStorage" }
+  )
+)
+
+export default useChannelsStore
