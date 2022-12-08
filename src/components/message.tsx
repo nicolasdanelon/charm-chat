4185b6d5841c01b6f94ea67d22e5786792de3729
Nicolas Danelon <nicolasdanelon@gmail.com>
2022-12-08 15:21:35 -0300

    💄 useMemo?

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -7,14 +7,16 @@ type MessageProps = {
   time: Date
 }
 
-function returnAvatar(name) {
+function returnAvatar(name: string): string {
   return `https://www.gravatar.com/avatar/${Md5.hashStr(name)}?d=retro&f=y`
 }
 
 function Message({ name, content, time }: MessageProps) {
+  const avatar = useMemo(() => returnAvatar(name), [name])
+
   return (
     <div className="flex items-start mb-4 text-sm">
-      <img src={returnAvatar(name)} className="w-10 h-10 rounded mr-3" />
+      <img src={avatar} className="w-10 h-10 rounded mr-3" />
       <div className="flex-1 overflow-hidden">
         <div>
           <span className="font-bold capitalize">{name}</span>{" "}
