02ef1837ec9d504f7db2abb53bf471a8b292dc03
Maxi <maxiskell@gmail.com>
2022-12-22 14:07:41 +0000

    ✨ Online charmers (#21)
    * 💄 Charmer online status indicator
    
    * 💄 fix online/offline badge
    
    * 💄 adding "avatar"
    
    * ✨ Realtime user online status
    
    * 💄 Active looks like active
    
    Co-authored-by: Nicolas Danelon <nicolasdanelon@gmail.com>

--- src/components/direct-messages.tsx
+++ src/components/direct-messages.tsx
@@ -1,18 +1,50 @@
-import { useEffect } from "preact/hooks"
+import { RealtimeChannel } from "@supabase/supabase-js"
+import { useEffect, useState } from "preact/hooks"
+
 import useCharmersStore from "../stores/useCharmersStore"
 import useChannelsStore from "../stores/useChannelsStore"
 import useUserStore from "../stores/useUserStore"
+import { supabase } from "../supabaseClient"
+import CharmerRecord from "../types/charmer-record.type"
 
 export default function DirectMessages() {
   const { setCurrentChannelId, setCurrentChannelName } = useChannelsStore()
-  const { charmers, getCharmers, setSelectedCharmer, selectedCharmer } =
-    useCharmersStore()
+  const {
+    charmers,
+    getCharmers,
+    setSelectedCharmer,
+    selectedCharmer,
+    setCharmerOnlineStatus,
+  } = useCharmersStore()
   const { user } = useUserStore()
+  const [channel, setChannel] = useState<RealtimeChannel>()
 
   useEffect(() => {
     ;(async () => {
       await getCharmers()
     })()
+
+    const c = supabase
+      .channel("public:charmers")
+      .on<CharmerRecord>(
+        "postgres_changes",
+        {
+          event: "UPDATE",
+          schema: "public",
+          table: "charmers",
+        },
+        async (payload) => {
+          const { id, is_online } = payload.new
+          setCharmerOnlineStatus(id, is_online)
+        }
+      )
+      .subscribe()
+
+    setChannel(c)
+
+    return () => {
+      if (channel) supabase.removeChannel(channel)
+    }
   }, [])
 
   return (
@@ -42,7 +74,9 @@ export default function DirectMessages() {
       </div>
 
       {charmers.map((charmer) => {
-        const color = `text-${charmer.is_online ? "green" : "white"}`
+        const active = charmer.is_online
+          ? "text-green fill-current"
+          : "text-white stroke-current"
 
         if (charmer.id === user!.id) return null
 
@@ -54,19 +88,30 @@ export default function DirectMessages() {
               setCurrentChannelName(charmer.name)
             }}
             className={`
-              flex items-center py-1 px-4 opacity-50 w-full
-              cursor-pointer hover:bg-teal-500 hover:opacity-100
-              ${selectedCharmer === charmer.id && "bg-teal-500 opacity-100"}
+              flex items-center py-1 px-4 w-full
+              cursor-pointer charmer-item
+              ${selectedCharmer === charmer.id && "bg-teal-500"}
             `}
             key={charmer.id}
           >
-            <svg
-              className={`h-2 w-2 stroke-current ${color} mr-2`}
-              viewBox="0 0 22 22"
-            >
-              <circle cx="11" cy="11" r="9" fill="none" stroke-width="3" />
+            <svg className={`h-2 w-2 ${active} mr-2`} viewBox="-2 -2 23 23">
+              <circle
+                cx="10"
+                cy="10"
+                r="10"
+                fill={`${charmer.is_online ? "" : "none"}`}
+                stroke-width="3"
+              />
             </svg>
-            <span className="text-white capitalize">{charmer.name}</span>
+            <span
+              className={`text-white capitalize ${
+                selectedCharmer === charmer.id
+                  ? "opacity-100 hover:opacity-80"
+                  : "opacity-50 hover:opacity-100"
+              } `}
+            >
+              {charmer.name}
+            </span>
           </button>
         )
       })}
--- src/stores/useCharmersStore.ts
+++ src/stores/useCharmersStore.ts
@@ -13,16 +13,29 @@ type ChannelsStore = {
   clearCharmers: () => void
   selectedCharmer: string | null
   setSelectedCharmer: (id: string | null) => void
+  setCharmerOnlineStatus: (id: string, isOnline: boolean) => void
   getCharmers: () => Promise<void>
 }
 
 const useCharmersStore = create<ChannelsStore>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       charmers: [],
       selectedCharmer: null,
       setSelectedCharmer: (selectedCharmer: string | null) =>
         set({ selectedCharmer }),
+      setCharmerOnlineStatus: (id: string, isOnline: boolean) => {
+        const charmers = get().charmers
+        const index = charmers.findIndex((charmer) => charmer.id === id)
+
+        set({
+          charmers: [
+            ...charmers.slice(0, index),
+            { ...charmers[index], is_online: isOnline },
+            ...charmers.slice(index + 1),
+          ],
+        })
+      },
       clearCharmers: () => set({ charmers: [] }),
       getCharmers: async () => {
         const { data: charmers, error } = await supabase
